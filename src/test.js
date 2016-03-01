a14a3b8fd30d9de59c82a5f0b023a70ff581a4b9
Superloup10 <Superloup10@gmail.com>
2016-03-01 11:29:59 +0100

    Update server

--- src/test.js
+++ src/test.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
+	var page = url.parse(req.url).pathname;
+	var params = querystring.parse(url.parse(req.url).query);
+	console.log('Page \: ' + page);
+	console.log('Params \: ' + params + '\n');
 	res.writeHead(200, {"Content-Type":"text/html"});
-	res.end('<p>Bienvenue sur le <strong>serveur</strong>, ceci est une page de test</p>');
+	if(page == '/') {
+		res.write('<p>Bienvenue sur le <strong>serveur</strong>, ceci est la page d\'accueil</p>');
+	}
+	else if(page == '/admin') {
+		res.write('<p>Voici la page d\'administration !</p>');
+	}
+	else {
+		res.writeHead(404, {"Content-Type":"text/html"});
+		res.write('<h1>Erreur 404\:</h1><p>La page que vous avez demandé n\'existe pas.</p>');
+	}
+	res.end();
 });
 server.listen(8080);
