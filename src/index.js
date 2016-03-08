ad834aea7120e29de9c022bfc1ecc0ce1d9f024d
Superloup10 <Superloup10@gmail.com>
2016-03-08 10:41:43 +0100

    Update status

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ app/*.use(morgan('combined')
 	res.render("index.ejs"); // Render Main page
 }).get('/admin', function(req, res) { // Admin page
 	res.render("admin.ejs"); // Render Admin page
-}).get('/admin/status/:occuper', function(req, res) { // Status page
-	var num = ["0", "1", "2", "3", "4", "5", "6", "7"];
-	res.render("status.ejs", {num: num, occuper: req.params.occuper}); // Render Status page and send params temp
-}).use(function(req, res, next) {
+}).get('/admin/status', function(req, res) { // Status page
+	var num = ["0", "1", "2", "3", "4", "5", "6", "7"]; // Place of parking Number
+	var occuper = ["occuper", "non occuper"]; // Place of parking Status
+	res.render("status.ejs", {num: num, occuper: occuper}); // Render Status page and send params temp
+}).use(function(req, res, next) { // 404 Page
 	res.status(404).render("404.ejs"); // Render 404 page and send status 404
 });
 app.listen(8080); // Server Started on localhost:8080
