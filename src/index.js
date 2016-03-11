f8cc77889ba49b10f17343cc8b36fe0f1210fe69
Superloup10 <Superloup10@gmail.com>
2016-03-11 16:01:18 +0100

    Add socket.io

--- src/index.js
+++ src/index.js
@@ -22,16 +22,15 @@ app/*.use(morgan('combined')
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-	fs.readFile('./index.html', 'utf-8', function(error, content) {
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end(content);
-	});
-});
+var server = http.createServer(app);
 
 var io = require('socket.io')(server);
 io.on('connection',function(socket) {
 	socket.emit('message', 'Vous êtes bien connecté !');
+	
+	socket.on('message', function(message) {
+		console.log('Un client me parle ! Il me dit : ' + message);
+	});
 });
 
 server.listen(8080); // Server Started on localhost:8080
