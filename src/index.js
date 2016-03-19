21ef7dbd8312f34ea61901ac1ae04cfc73b66d0e
Superloup10 <Superloup10@gmail.com>
2016-03-20 00:24:39 +0100

    WIP BDD

--- src/index.js
+++ src/index.js
@@ -22,76 +22,68 @@ var session = require('cookie-session');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var ip = '192.168.1.37';
+var ip = '192.168.1.15';
 var port = '8080';
 
 var mysql = require('mysql');
 var bdd = mysql.createConnection( {
-        host : 'localhost',
-        user : 'parking',
-        password : 'parking',
-        database : 'parking',
-        charset : 'utf8'
+	multipleStatements: true,
+    host : 'localhost',
+    user : 'parking',
+    password : 'parking',
+    database : 'parking',
+    charset : 'utf8'
 });
-                                
+
 bdd.connect();
 
-app.use(logger('dev', // On utilise un logger
-	{ stream: accessLogStream } // On enregistre les logs dans un fichier
-)).use(express.static(__dirname + '/public') // Dossier où se trouve les images
-).use(favicon(__dirname + '/public/favicon.ico') // Lien vers le favicon
-).use(session({ secret: 'secret' })
-).use(function(req, res, next) {
-	if(typeof(req.session.register) == 'undefined') {
-		req.session.register = [];
-	}
-	next();
-}).get('/', function(req, res) { // Page principale
-	res.render("index.ejs", { ip: ip, port: port }); // Rendu de la page principale
-}).get('/inscription', function(req, res) { // Page d'inscription
-	res.render("inscription.ejs"); //Rendu de la page d'inscription
-}).post('/inscription/personnel', urlencodedParser, function(req, res) {
-	if(req.body.name != '' || req.body.firstname != '' || req.body.username != '' || req.body.password != '' || req.body.fonction != '') {
-		req.session.register.push(req.body.name);
-		req.session.register.push(req.body.firstname);
-		req.session.register.push(req.body.username);
-		req.session.register.push(req.body.password);
-		req.session.register.push(req.body.fonction);
-	}
-	res.redirect('/inscription');
-}).get('/admin', function(req, res) { // Page Admin
-	res.render("admin.ejs"); // Rendu de la page Admin
-}).get('/admin/status', function(req, res) { // Page status
-	var num = ["0", "1", "2", "3", "4", "5", "6", "7"]; // Numéro de la place de parking
-	var occuper = ["occuper", "non occuper"]; // Status de la place de parking
-	/*bdd.query('SELECT reload FROM subscribe', function(err, result) {
-		if(err) throw err;
-
-	        console.log('Reload OK');*/
-		res.render("status.ejs", {user: req.session.register, num: num, occuper: occuper, hour : '00', minute: '00', second: '00', reload: 100/*result*//*bdd.query('SELECT reload FROM subscribe',function(err, rows,fields) {
-			if(err) throw err;
-		
-			console.log('Reload OK');
-		})*/, cost: 100}); // Rendu de la page status et envoit de données temporaires
-	/*});*/
-}).use(function(req, res, next) { // Page 404
-	res.status(404).render("404.ejs"); // Rendu de la page 404 et envoit du status 404
-});
-
-io.on('connection',function(socket) { // Event connection au serveur
-	socket.emit('message', 'Vous êtes bien connecté !'); // On indique au client qu'il est bien connecté
-	
-	socket.on('message', function(message) { // On attend un message du client
-		console.log('Un client me parle ! Il me dit : ' + message); // Le client a envoyé un message
-		socket.emit('message', 'Oui, ça va, mais arrête de m\'embêter'); // On envoit un message au client
-	});
-});
-
-/*connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+bdd.query('SELECT * FROM subscribe;SELECT * FROM parking', function(err, results, fields) {
 	if(err) throw err;
 
-	console.log('The solution is: ', rows[0].solution);
-});*/
+    console.log(results);
+
+	app.use(logger('dev', // On utilise un logger
+		{ stream: accessLogStream } // On enregistre les logs dans un fichier
+	)).use(express.static(__dirname + '/public') // Dossier où se trouve les images
+	).use(favicon(__dirname + '/public/favicon.ico') // Lien vers le favicon
+	).use(session({ secret: 'secret' })
+	).use(function(req, res, next) {
+		if(typeof(req.session.register) == 'undefined') {
+			req.session.register = [];
+		}
+		next();
+	}).get('/', function(req, res) { // Page principale
+		res.render("index.ejs", { ip: ip, port: port }); // Rendu de la page principale
+	}).get('/inscription', function(req, res) { // Page d'inscription
+		res.render("inscription.ejs"); //Rendu de la page d'inscription
+	}).post('/inscription/personnel', urlencodedParser, function(req, res) {
+		if(req.body.name != '' || req.body.firstname != '' || req.body.username != '' || req.body.password != '' || req.body.fonction != '') {
+			req.session.register.push(req.body.name);
+			req.session.register.push(req.body.firstname);
+			req.session.register.push(req.body.username);
+			req.session.register.push(req.body.password);
+			req.session.register.push(req.body.fonction);
+		}
+		res.redirect('/inscription');
+	}).get('/admin', function(req, res) { // Page Admin
+		res.render("admin.ejs"); // Rendu de la page Admin
+	}).get('/admin/status', function(req, res) { // Page status
+		var num = ["0", "1", "2", "3", "4", "5", "6", "7"]; // Numéro de la place de parking
+		var occuper = ["occuper", "non occuper"]; // Status de la place de parking
+		res.render("status.ejs", {occuper: occuper, hour : '00', minute: '00', second: '00', result: results}); // Rendu de la page status et envoit de données temporaires
+	}).use(function(req, res, next) { // Page 404
+		res.status(404).render("404.ejs"); // Rendu de la page 404 et envoit du status 404
+	});
+	
+	io.on('connection',function(socket) { // Event connection au serveur
+		socket.emit('message', 'Vous êtes bien connecté !'); // On indique au client qu'il est bien connecté
+		
+		socket.on('message', function(message) { // On attend un message du client
+			console.log('Un client me parle ! Il me dit : ' + message); // Le client a envoyé un message
+			socket.emit('message', 'Oui, ça va, mais arrête de m\'embêter'); // On envoit un message au client
+		});
+	});
+});
 
 bdd.end();
 
